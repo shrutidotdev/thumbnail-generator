cc11cd933dd11721e7b368fc2bb3d8dbdd152462
shruti singh <shrutisinghskr@gmail.com>
2025-12-27 02:47:59 +0530

    fix(thumbnail-creator): downgraded

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,3 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/app/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
